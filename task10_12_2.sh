39b5661431cbfd59405e1e7dea088a441d5898ed
Eugeniy Khvastunov <khvastunov@gmail.com>
2018-05-20 23:25:03 +0300

    Adding OpenSSL

--- task10_12_2.sh
+++ task10_12_2.sh
@@ -24,12 +24,53 @@ echo "===CONFIG START===
 SCRPATH: $SCRPATH
 CONFIGFILE: $CONFIGFILE
 # Host parameters
-EXTERNAL_IP=10.14.254.15
-HOST_NAME=docker-vm.domain.tld
+EXTERNAL_IP: $EXTERNAL_IP
+HOST_NAME: $HOST_NAME
 # Docker parameters
-NGINX_IMAGE="nginx:1.13"
-APACHE_IMAGE="httpd:2.4"
-NGINX_PORT=17080
-NGINX_LOG_DIR=/srv/log/nginx
+NGINX_IMAGE: $NGINX_IMAGE
+APACHE_IMAGE: $APACHE_IMAGE
+NGINX_PORT: $NGINX_PORT
+NGINX_LOG_DIR: $NGINX_LOG_DIR
 ===CONFIG END==="
+#
+echo "Step 1: Setting up stuff..."
+echo "Adding Docker repo key..."
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo "Adding Docker repo..."
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+echo "Installing docker-ce and docker-compose..."
+sudo apt-get install docker-ce docker-compose -y
+echo "Installing OpenSSL..."
+sudo apt-get install openssl -y
+#
+echo "Generating certs..."
+/bin/mkdir -p /etc/ssl/certs
 
+echo "[ req ]
+default_bits = 2048
+prompt = no
+default_md = sha256
+x509_extensions = v3_req
+distinguished_name = dn
+
+[ dn ]
+C = UA
+ST = Kharkov
+L = Kharkov
+O = Mirantis
+OU=Internship
+emailAddress = khvastunov@gmail.com
+CN = vm1
+
+[ san ]
+subjectAltName = \"DNS:vm1,IP:$NGINX_IP\"
+" > /etc/ssl/vm1.cnf
+
+openssl genrsa -out /etc/ssl/certs/root-ca.key 4096
+/usr/bin/openssl req -x509 -new -nodes -key /etc/ssl/certs/root-ca.key -sha256 -days 365 -out /etc/ssl/certs/root-ca.crt -subj "/C=UA/ST=Kharkov/L=Kharkov/O=Mirantis/OU=Internship/CN=vm1/" -reqexts san -extensions san -config /etc/ssl/vm1.cnf
+/usr/bin/openssl genrsa -out /etc/ssl/certs/web.key 2048
+/usr/bin/openssl req -new -out /etc/ssl/certs/web.csr -key /etc/ssl/certs/web.key -subj "/C=UA/ST=Kharkov/L=Kharkov/O=Mirantis/OU=Internship/CN=vm1/" -reqexts san -extensions san -config /etc/ssl/vm1.cnf
+/usr/bin/openssl x509 -req -in /etc/ssl/certs/web.csr -CA /etc/ssl/certs/root-ca.crt -CAkey /etc/ssl/certs/root-ca.key -CAcreateserial -out /etc/ssl/certs/web.crt
+
+cat /etc/ssl/certs/root-ca.crt /etc/ssl/certs/web.crt > /etc/ssl/certs/chain.pem
