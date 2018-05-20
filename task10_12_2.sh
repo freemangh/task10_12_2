a65285f62f6b67595ac3910915d6dd335ccca2e2
Eugeniy Khvastunov <khvastunov@gmail.com>
2018-05-21 00:14:25 +0300

    Fix some bugs

--- task10_12_2.sh
+++ task10_12_2.sh
@@ -45,7 +45,7 @@ echo "Installing OpenSSL..."
 sudo apt-get install openssl -y
 #
 echo "Generating certs..."
-/bin/mkdir -p /etc/ssl/certs
+mkdir -vp $SCRPATH'certs'
 
 echo "[ req ]
 default_bits = 2048
@@ -67,41 +67,91 @@ CN = $HOST_NAME
 subjectAltName = \"DNS:$HOST_NAME,IP:$EXTERNAL_IP\"
 " > /etc/ssl/docker.cnf
 
-openssl genrsa -out /etc/ssl/certs/root-ca.key 4096
-/usr/bin/openssl req -x509 -new -nodes -key /etc/ssl/certs/root-ca.key -sha256 -days 365 -out /etc/ssl/certs/root-ca.crt -subj "/C=UA/ST=Kharkov/L=Kharkov/O=Mirantis/OU=Internship/CN=$HOST_NAME/" -reqexts san -extensions san -config /etc/ssl/docker.cnf
-/usr/bin/openssl genrsa -out /etc/ssl/certs/web.key 2048
-/usr/bin/openssl req -new -out /etc/ssl/certs/web.csr -key /etc/ssl/certs/web.key -subj "/C=UA/ST=Kharkov/L=Kharkov/O=Mirantis/OU=Internship/CN=$HOST_NAME/" -reqexts san -extensions san -config /etc/ssl/docker.cnf
-/usr/bin/openssl x509 -req -in /etc/ssl/certs/web.csr -CA /etc/ssl/certs/root-ca.crt -CAkey /etc/ssl/certs/root-ca.key -CAcreateserial -out /etc/ssl/certs/web.crt
+openssl genrsa -out $SCRPATH'certs/root.key' 4096
+/usr/bin/openssl req -x509 -new -nodes -key $SCRPATH'certs/root.key' -sha256 -days 365 -out $SCRPATH'certs/root.crt' -subj "/C=UA/ST=Kharkov/L=Kharkov/O=Mirantis/OU=Internship/CN=$HOST_NAME/" -reqexts san -extensions san -config /etc/ssl/docker.cnf
+/usr/bin/openssl genrsa -out $SCRPATH'certs/web.key' 2048
+/usr/bin/openssl req -new -out $SCRPATH'certs/web.csr' -key $SCRPATH'certs/web.key' -subj "/C=UA/ST=Kharkov/L=Kharkov/O=Mirantis/OU=Internship/CN=$HOST_NAME/" -reqexts san -extensions san -config /etc/ssl/docker.cnf
+/usr/bin/openssl x509 -req -in $SCRPATH'certs/web.csr' -CA $SCRPATH'certs/root.crt' -CAkey $SCRPATH'certs/root.key' -CAcreateserial -out $SCRPATH'certs/web.crt'
 
-cat /etc/ssl/certs/root-ca.crt /etc/ssl/certs/web.crt > /etc/ssl/certs/chain.pem
+cat $SCRPATH'certs/root.crt' $SCRPATH'certs/web.crt' > $SCRPATH'certs/chain.pem'
 #
 echo "Creating nginx log dir:"
 mkdir -vp ${NGINX_LOG_DIR}
 echo "Creating nginx sites dir:"
-mkdir -vp $SCRPATH'sites-enabled'
+mkdir -vp $SCRPATH'etc'
 #---<START: nginx default sitetemplate>---
-echo "server {
-	listen 443 ssl default_server;
-	# listen [::]:443 ssl default_server;
+echo "user www-data;
+worker_processes auto;
+pid /run/nginx.pid;
 
-	#root /var/www/html;
+events {
+        worker_connections 768;
+        # multi_accept on;
+}
 
-	# Add index.php to the list if you are using PHP
-	#index index.html index.htm index.nginx-debian.html;
+http {
 
-	server_name _;
-	ssl_certificate /etc/nginx/certs/chain.pem;
-	ssl_certificate_key /etc/nginx/certs/root-ca.key;
+        ##
+        # Basic Settings
+        ##
 
-	location / {
-		proxy_set_header HOST \$host;
-		proxy_set_header X-Forwarded-Proto \$scheme;
-		proxy_set_header X-Real-IP \$remote_addr;
-		proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-		proxy_pass http://apache:80\$request_uri;
+        sendfile on;
+        tcp_nopush on;
+        tcp_nodelay on;
+        keepalive_timeout 65;
+        types_hash_max_size 2048;
+        # server_tokens off;
+
+        # server_names_hash_bucket_size 64;
+        # server_name_in_redirect off;
+
+        include /etc/nginx/mime.types;
+        default_type application/octet-stream;
+
+        ##
+        # SSL Settings
+        ##
+
+        ssl_protocols TLSv1 TLSv1.1 TLSv1.2; # Dropping SSLv3, ref: POODLE
+        ssl_prefer_server_ciphers on;
+
+        ##
+        # Logging Settings
+        ##
+
+        access_log /var/log/nginx/access.log;
+        error_log /var/log/nginx/error.log;
+
+        ##
+        # Gzip Settings
+        ##
+
+        gzip on;
+        gzip_disable "msie6";
+
+	server {
+		listen 443 ssl default_server;
+		# listen [::]:443 ssl default_server;
+	
+		#root /var/www/html;
+
+		# Add index.php to the list if you are using PHP
+		#index index.html index.htm index.nginx-debian.html;
+
+		server_name _;
+		ssl_certificate /etc/nginx/certs/chain.pem;
+		ssl_certificate_key /etc/nginx/certs/root.key;
+
+		location / {
+			proxy_set_header HOST \$host;
+			proxy_set_header X-Forwarded-Proto \$scheme;
+			proxy_set_header X-Real-IP \$remote_addr;
+			proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+			proxy_pass http://apache:8080\$request_uri;
+		}
+
 	}
-
-}" > $SCRPATH'sites-enabled/default'
+}" > $SCRPATH'etc'
 #---<END: nginx default sitetemplate>---
 #
 #---<START: Docker compose template>---
@@ -111,8 +161,8 @@ services:
     image: ${NGINX_IMAGE}
     hostname: nginx
     volumes:
-      - .sites-enabled:/etc/nginx/sites-enabled
-      - /etc/ssl/certs:/etc/nginx/certs
+      - .etc:/etc/nginx/sites-enabled
+      - .certs:/etc/nginx/certs
       - ${NGINX_LOG_DIR}:/var/log/nginx
   ports:
       - "${NGINX_PORT}:443"
@@ -120,7 +170,7 @@ services:
     image: ${APACHE_IMAGE}
     hostname: apache
     ports:
-      - "80:80" " > $SCRPATH'docker-compose.yml'
+      - "8080:80" " > $SCRPATH'docker-compose.yml'
 #---<END: Docker compose template>---
 echo "Deploying containers..."
 docker-compose up -d
